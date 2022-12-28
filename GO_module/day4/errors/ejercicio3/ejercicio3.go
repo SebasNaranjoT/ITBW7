3f74e45271a794baa710916a0afb351fcf62ccbd
SebasNaranjoT <sebastian.naranjo@mercadolibre.com.co>
2022-12-28 12:09:01 -0500

    solved ejercicio3 from errors class

---
+++ GO_module/day4/errors/ejercicio3/ejercicio3.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type customError struct {
+	msg string
+}
+
+func (c customError) Error() string {
+	return "Error: El salario es menor a 10000"
+}
+
+func NewCustom(message string) error {
+	return &customError{msg: message}
+}
+
+var otherCustom = NewCustom("Otro mensaje de error custom")
+
+func main() {
+
+	var salary int = 10000
+	err := validacionSalario(salary)
+	if err != nil {
+		if errors.Is(err, otherCustom) {
+			fmt.Println(errors.New("Error: El salario es menor a 10000"))
+		}
+	} else {
+		fmt.Println("Debe pagar Impuesto")
+	}
+}
+
+func validacionSalario(salary int) error {
+	if salary <= 10000 {
+		return otherCustom
+	}
+	return nil
+}
